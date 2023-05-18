233ede4891bea4a6747fa04b14a2fc3d5b000b38
Harsh  Joshi <78259306+harsh0506@users.noreply.github.com>
2023-05-18 16:19:31 +0530

    Update Auth.jsx

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -157,7 +157,7 @@ function Auth() {
         <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
           <div
             class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-            style={{ backgroundImage: "url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg')" }}
+            style={{ backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/notes-5bb98.appspot.com/o/To%20do%20list-pana.png?alt=media&token=34dd29a0-c7b6-45b6-9ee0-048408dd9055')" }}
           ></div>
         </div>
       </div>
